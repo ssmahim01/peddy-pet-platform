7b5714687f8082b047d0410d97c80a6749b52fca
ssmahim01 <mahimzayn666@gmail.com>
2024-10-06 01:37:36 +0600

    Updated Script file and Added Footer with made it responsive

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('mahim');
+/* Load all Pets */
+
+const loadAllPets = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
+    const data = await response.json();
+    displayPosts(data.pets);
+};
+
+/* Load all Pets categories */
+
+const loadCategories = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
+    const data = res.json();
+    console.log(data.categories);
+}
+
+loadAllPets();
+
+const pets = {
+    petId: 1, 
+    breed: 'Golden Retriever', 
+    category: 'Dog', 
+    date_of_birth: '2023-01-15', 
+    price: 1200,
+    vaccinated_status: "Fully"
+};
