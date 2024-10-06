b65a182a162e10c18375a26bc8ccad39733e0f38
ssmahim01 <mahimzayn666@gmail.com>
2024-10-06 12:53:45 +0600

    Updated HTML file and Append category Buttons in Best Friend section

--- script.js
+++ script.js
@@ -3,18 +3,39 @@
 const loadAllPets = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
     const data = await response.json();
-    displayPosts(data.pets);
+    console.log(data.pets);
 };
 
 /* Load all Pets categories */
 
 const loadCategories = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
-    const data = res.json();
-    console.log(data.categories);
+    const data = await res.json();
+    displayCategories(data.categories);
+};
+
+/* Display Categories */
+
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('categories');
+
+    categories.forEach(category => {
+        console.log(category);
+
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <button class="py-4 px-16 rounded-2xl border border-slate-400 font-bold flex gap-4 items-center">
+         <img class="w-12" src=${category.category_icon}/>
+         <p class="text-2xl">${category.category}</p>
+        </button>
+        `
+        categoriesContainer.appendChild(newDiv);
+    });
+
 }
 
 loadAllPets();
+loadCategories();
 
 const pets = {
     petId: 1, 
