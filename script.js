53a4a9a9970994a15583803e4b3cef794264257c
ssmahim01 <mahimzayn666@gmail.com>
2024-10-06 13:35:23 +0600

    Added Deal Section and Updated Script file of JS

--- script.js
+++ script.js
@@ -3,10 +3,16 @@
 const loadAllPets = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
     const data = await response.json();
-    console.log(data.pets);
+    displayAllPets(data.pets);
 };
 
-/* Load all Pets categories */
+/* Display All Pets */
+
+const displayAllPets = (pets) => {
+   console.log(pets);
+}
+
+/* Load Categories */
 
 const loadCategories = async() =>{
     const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
@@ -20,11 +26,9 @@ const displayCategories = (categories) => {
     const categoriesContainer = document.getElementById('categories');
 
     categories.forEach(category => {
-        console.log(category);
-
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
-        <button class="py-4 px-16 rounded-2xl border border-slate-400 font-bold flex gap-4 items-center">
+        <button class="py-4 px-14 rounded-2xl border border-slate-200 font-bold flex gap-4 items-center hover:rounded-full hover:bg-slate-200 hover:border-2 hover:border-btnPrimary">
          <img class="w-12" src=${category.category_icon}/>
          <p class="text-2xl">${category.category}</p>
         </button>
@@ -32,16 +36,16 @@ const displayCategories = (categories) => {
         categoriesContainer.appendChild(newDiv);
     });
 
-}
+};
 
 loadAllPets();
 loadCategories();
 
-const pets = {
-    petId: 1, 
-    breed: 'Golden Retriever', 
-    category: 'Dog', 
-    date_of_birth: '2023-01-15', 
-    price: 1200,
-    vaccinated_status: "Fully"
-};
+// const pets = {
+//     petId: 1, 
+//     breed: 'Golden Retriever', 
+//     category: 'Dog', 
+//     date_of_birth: '2023-01-15', 
+//     price: 1200,
+//     vaccinated_status: "Fully"
+// };
