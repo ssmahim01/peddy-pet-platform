33341b03d0098de8c66a868160c1307d2eee4937
ssmahim01 <mahimzayn666@gmail.com>
2024-10-07 00:51:02 +0600

    Added Modal and Edited contents in HTML and JS file

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const displayAllPets = (pets) => {
     const div = document.createElement('div');
     div.innerHTML = `
             <div class="border border-gray-400 rounded-xl p-6 space-y-3">
-            <div class="h-40">
+            <div class="lg:h-40">
             <img class="rounded-lg w-full h-full object-cover" src=${pet.image}/>
             </div>
                     <h3 class="font-fontInter text-xl font-bold">${pet.pet_name}</h3>
@@ -33,7 +33,7 @@ const displayAllPets = (pets) => {
 
                   <button onclick="loadAdopt()" class="py-2 px-4 border border-slate-400 rounded-lg text-btnPrimary text-lg font-bold">Adopt</button>
 
-                  <button onclick="loadDetails()" class="py-2 px-4 border border-slate-400 rounded-lg text-btnPrimary text-lg font-bold">Details</button>
+                  <button onclick="loadDetailsPet('${pet.petId}')" class="py-2 px-4 border border-slate-400 rounded-lg text-btnPrimary text-lg font-bold">Details</button>
                     </div>
                   </div>
     `
@@ -77,10 +77,9 @@ const displayCategories = (categories) => {
     const categoriesContainer = document.getElementById('categories');
 
     categories.forEach(category => {
-      console.log(category);
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
-        <button onclick="controlCategoryContent('${category.category}')" class="py-4 px-14 rounded-2xl border border-slate-200 font-fontInter font-bold flex gap-4 items-center hover:rounded-full hover:bg-slate-200 hover:border-2 hover:border-btnPrimary">
+        <button onclick="controlCategoryContent('${category.category}')" class="py-4 lg:px-20 md:px-16 px-5 rounded-2xl border border-slate-200 font-fontInter font-bold flex gap-4 items-center hover:rounded-full hover:bg-slate-200 hover:border-2 hover:border-btnPrimary">
          <img class="w-12" src=${category.category_icon}/>
          <p class="text-2xl">${category.category}</p>
         </button>
@@ -88,6 +87,49 @@ const displayCategories = (categories) => {
         categoriesContainer.appendChild(newDiv);
     });
 
+};
+
+/* Load details of Pet */
+
+const loadDetailsPet = async(petId) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`);
+
+  const data = await res.json();
+  displayPetDetails(data.petData);
+};
+
+const displayPetDetails = (petData) => {
+  console.log(petData);
+  const contentOfModal = document.getElementById('show-content');
+
+  contentOfModal.innerHTML = `
+  <div class="space-y-4">
+    <div class="space-y-3 w-full h-64">
+      <img class="w-full rounded-lg h-full" src=${petData.image}/>
+      </div>
+          <h3 class="text-xl font-fontInter font-extrabold pt-3">${petData.pet_name}</h3>
+                <div class="flex flex-row sm:gap-5 gap-2 text-gray-500 font-medium">
+                      <div class="space-y-2">
+                        <p><i class="fa-solid fa-qrcode"></i>  Breed: ${petData?.breed || "Unknown"}</p>
+                        <p><i class="fa-solid fa-mercury"></i>  Gender: ${petData?.gender || "Unknown"}</p>
+                        <p><i class="fa-regular fa-calendar"></i>  Vaccinated Status: ${petData?.vaccinated_status || "Unknown"}</p>
+                      </div>
+                      <div class="space-y-2">
+                       <p><i class="fa-regular fa-calendar"></i>  Birth: ${petData?.date_of_birth || "Unknown"}</p>
+                        <p><i class="fa-solid fa-dollar-sign"></i>  Price: ${petData?.price || "Unknown"}</p>
+                      </div>
+                    </div>
+
+                    <hr class="text-gray-400"/>
+
+                    <div class="font-fontInter">
+                      <h3 class="font-bold pb-3">Details Information</h3>
+                      <p class="opacity-80 font-normal">${petData.pet_details}</p>
+                    </div>
+                  </div>
+                  `;
+
+                  document.getElementById('showDetails').showModal();
 };
 
 loadAllPets();
